3a6ec4da62370886f462c436d05ba4ab868611f6
Qin Lyu <qlyu@g.harvard.edu>
2016-11-30 08:32:59 -0500

    Refactoring rbtree, and make it pip installable

--- cs207rbtree/tree.py
+++ cs207rbtree/tree.py
@@ -1,22 +1,27 @@
-import os
-import struct
-import portalocker
+"""
+TODO:
+1. Add BST module, and refactoring red-black tree
+2. Unittest
+3. 
+"""
 import pickle
+
 
 class Color(object):
     RED = 'red'
     BLACK = 'black'
 
+
 class ValueRef(object):
     " a reference to a string value on disk"
     def __init__(self, referent=None, address=0):
-        self._referent = referent #value to store
-        self._address = address #address to store at
-        
+        self._referent = referent  # value to store
+        self._address = address  # address to store at
+
     @property
     def address(self):
         return self._address
-    
+
     def prepare_to_store(self, storage):
         pass
 
@@ -28,7 +33,6 @@ class ValueRef(object):
     def bytes_to_referent(bytes):
         return bytes.decode('utf-8')
 
-    
     def get(self, storage):
         "read bytes for value from disk"
         if self._referent is None and self._address:
@@ -37,15 +41,16 @@ class ValueRef(object):
 
     def store(self, storage):
         "store bytes for value to disk"
-        #called by BinaryNode.store_refs
+        # called by BinaryNode.store_refs
         if self._referent is not None and not self._address:
             self.prepare_to_store(storage)
             self._address = storage.write(self.referent_to_bytes(self._referent))
 
+
 class BinaryNodeRef(ValueRef):
     "reference to a btree node on disk"
-    
-    #calls the BinaryNode's store_refs
+
+    # calls the BinaryNode's store_refs
     def prepare_to_store(self, storage):
         "have a node store its refs"
         if self._referent:
@@ -73,8 +78,6 @@ class BinaryNodeRef(ValueRef):
             BinaryNodeRef(address=d['right']),
             ValueRef(address=d['color'])
         )
-    
-
 
 
 class BinaryNode(object):
@@ -99,15 +102,13 @@ class BinaryNode(object):
     def store_refs(self, storage):
         "method for a node to store all of its stuff"
         self.value_ref.store(storage)
-        #calls BinaryNodeRef.store. which calls
-        #BinaryNodeRef.prepate_to_store
-        #which calls this again and recursively stores
-        #the whole tree
+        # calls BinaryNodeRef.store. which calls
+        # BinaryNodeRef.prepate_to_store
+        # which calls this again and recursively stores
+        # the whole tree
         self.left_ref.store(storage)
         self.right_ref.store(storage)
         self.color_ref.store(storage)
-
-
 
     def is_empty(self):
         return False
@@ -118,7 +119,6 @@ class BinaryNode(object):
     def is_red(self):
         return self.color_ref == ValueRef(Color.RED)
 
-    
     def blacken(self):
         if self.is_red():
             return BinaryNode.from_node(
@@ -126,7 +126,6 @@ class BinaryNode(object):
                 color_ref=ValueRef(Color.BLACK)
             )
         return self
-
 
     def rotate_left(self, storage):
         rchild = self.right_ref.get(storage)
@@ -143,7 +142,6 @@ class BinaryNode(object):
             rchild.color_ref
         )
 
-
     def rotate_right(self, storage):
         lchild = self.left_ref.get(storage)
         llchild = lchild.left_ref.get(storage)
@@ -159,7 +157,6 @@ class BinaryNode(object):
             lchild.color_ref
         )
 
-
     def recolored(self, storage):
         lchild = self.left_ref.get(storage)
         rchild = self.right_ref.get(storage)
@@ -169,7 +166,6 @@ class BinaryNode(object):
             right_ref=BinaryNodeRef(referent=rchild.blacken()),
             color_ref=ValueRef(Color.RED)
         )
-
 
     def balance(self, storage):
         lchild = self.left_ref.get(storage)
@@ -198,11 +194,10 @@ class BinaryNode(object):
                 return self.rotate_left(storage).recolored(storage)
             if rlchild.is_red():
                 return BinaryNode.from_node(
-                    self, 
+                    self,
                     right_ref=BinaryNodeRef(referent=rchild.rotate_right(storage))
                 ).rotate_left(storage).recolored(storage)
         return self
-
 
     def update(self, node, storage):
         lchild = self.left_ref.get(storage)
@@ -219,7 +214,6 @@ class BinaryNode(object):
             right_ref=BinaryNodeRef(referent=rchild.update(node, storage).balance(storage))
         ).balance(storage)
 
-
     def insert(self, key, value_ref, storage):
         return self.update(
             BinaryNode(
@@ -230,7 +224,6 @@ class BinaryNode(object):
                 color_ref=ValueRef(Color.RED)
             ), storage
         ).blacken()
-
 
 
 class EmptyNode(BinaryNode):
@@ -267,7 +260,6 @@ class EmptyNode(BinaryNode):
         return ValueRef()
 
 
-
 class BinaryTree(object):
     "Immutable Binary Tree class. Constructs new tree on changes"
     def __init__(self, storage):
@@ -276,9 +268,9 @@ class BinaryTree(object):
 
     def commit(self):
         "changes are final only when committed"
-        #triggers BinaryNodeRef.store
+        # triggers BinaryNodeRef.store
         self._tree_ref.store(self._storage)
-        #make sure address of new tree is stored
+        # make sure address of new tree is stored
         self._storage.commit_root_address(self._tree_ref.address)
 
     def _refresh_tree_ref(self):
@@ -288,14 +280,14 @@ class BinaryTree(object):
 
     def get(self, key):
         "get value for a key"
-        #your code here
-        #if tree is not locked by another writer
-        #refresh the references and get new tree if needed
+        # your code here
+        # if tree is not locked by another writer
+        # refresh the references and get new tree if needed
         if not self._storage.locked:
             self._refresh_tree_ref()
-        #get the top level node
+        # get the top level node
         node = self._follow(self._tree_ref)
-        #traverse until you find appropriate node
+        # traverse until you find appropriate node
         while node is not None:
             if key < node.key:
                 node = self._follow(node.left_ref)
@@ -307,29 +299,26 @@ class BinaryTree(object):
 
     def set(self, key, value):
         "set a new value in the tree. will cause a new tree"
-        #try to lock the tree. If we succeed make sure
-        #we dont lose updates from any other process
+        # try to lock the tree. If we succeed make sure
+        # we dont lose updates from any other process
         if self._storage.lock():
             self._refresh_tree_ref()
-        #get current top-level node and make a value-ref
+        # get current top-level node and make a value-ref
         node = self._follow(self._tree_ref)
         value_ref = ValueRef(value)
-        #insert and get new tree ref
+        # insert and get new tree ref
         self._tree_ref = self._insert(node, key, value_ref)
-        
-    
+
     def _insert(self, node, key, value_ref):
         "insert a new node creating a new path from root"
-        #create a tree ifnthere was none so far
+        # create a tree ifnthere was none so far
         if node is None:
             new_node = EmptyNode().insert(key, value_ref, self._storage)
 
         else:
             new_node = node.insert(key, value_ref, self._storage)
-        
+
         return BinaryNodeRef(referent=new_node)
-
-
 
     def delete(self, key):
         "delete node with key, creating new tree and path"
@@ -337,7 +326,7 @@ class BinaryTree(object):
             self._refresh_tree_ref()
         node = self._follow(self._tree_ref)
         self._tree_ref = self._delete(node, key)
-        
+
     def _delete(self, node, key):
         "underlying delete implementation"
         if node is None:
@@ -373,149 +362,12 @@ class BinaryTree(object):
 
     def _follow(self, ref):
         "get a node from a reference"
-        #calls BinaryNodeRef.get
+        # calls BinaryNodeRef.get
         return ref.get(self._storage)
-    
+
     def _find_max(self, node):
         while True:
             next_node = self._follow(node.right_ref)
             if next_node is None:
                 return node
             node = next_node
-
-
-class Storage(object):
-    SUPERBLOCK_SIZE = 4096
-    INTEGER_FORMAT = "!Q"
-    INTEGER_LENGTH = 8
-
-    def __init__(self, f):
-        self._f = f
-        self.locked = False
-        #we ensure that we start in a sector boundary
-        self._ensure_superblock()
-
-    def _ensure_superblock(self):
-        "guarantee that the next write will start on a sector boundary"
-        self.lock()
-        self._seek_end()
-        end_address = self._f.tell()
-        if end_address < self.SUPERBLOCK_SIZE:
-            self._f.write(b'\x00' * (self.SUPERBLOCK_SIZE - end_address))
-        self.unlock()
-
-    def lock(self):
-        "if not locked, lock the file for writing"
-        if not self.locked:
-            portalocker.lock(self._f, portalocker.LOCK_EX)
-            self.locked = True
-            return True
-        else:
-            return False
-
-    def unlock(self):
-        if self.locked:
-            self._f.flush()
-            portalocker.unlock(self._f)
-            self.locked = False
-
-    def _seek_end(self):
-        self._f.seek(0, os.SEEK_END)
-
-    def _seek_superblock(self):
-        "go to beginning of file which is on sec boundary"
-        self._f.seek(0)
-
-    def _bytes_to_integer(self, integer_bytes):
-        return struct.unpack(self.INTEGER_FORMAT, integer_bytes)[0]
-
-    def _integer_to_bytes(self, integer):
-        return struct.pack(self.INTEGER_FORMAT, integer)
-
-    def _read_integer(self):
-        return self._bytes_to_integer(self._f.read(self.INTEGER_LENGTH))
-
-    def _write_integer(self, integer):
-        self.lock()
-        self._f.write(self._integer_to_bytes(integer))
-
-    def write(self, data):
-        "write data to disk, returning the adress at which you wrote it"
-        #first lock, get to end, get address to return, write size
-        #write data, unlock <==WRONG, dont want to unlock here
-        #your code here
-        self.lock()
-        self._seek_end()
-        object_address = self._f.tell()
-        self._write_integer(len(data))
-        self._f.write(data)
-        return object_address
-
-    def read(self, address):
-        self._f.seek(address)
-        length = self._read_integer()
-        data = self._f.read(length)
-        return data
-
-    def commit_root_address(self, root_address):
-        self.lock()
-        self._f.flush()
-        #make sure you write root address at position 0
-        self._seek_superblock()
-        #write is atomic because we store the address on a sector boundary.
-        self._write_integer(root_address)
-        self._f.flush()
-        self.unlock()
-
-    def get_root_address(self):
-        #read the first integer in the file
-        #your code here
-        self._seek_superblock()
-        root_address = self._read_integer()
-        return root_address
-
-    def close(self):
-        self.unlock()
-        self._f.close()
-
-    @property
-    def closed(self):
-        return self._f.closed
-
-
-class DBDB(object):
-
-    def __init__(self, f):
-        self._storage = Storage(f)
-        self._tree = BinaryTree(self._storage)
-
-    def _assert_not_closed(self):
-        if self._storage.closed:
-            raise ValueError('Database closed.')
-
-    def close(self):
-        self._storage.close()
-
-    def commit(self):
-        self._assert_not_closed()
-        self._tree.commit()
-
-    def get(self, key):
-        self._assert_not_closed()
-        return self._tree.get(key)
-
-    def set(self, key, value):
-        self._assert_not_closed()
-        return self._tree.set(key, value)
-
-    def delete(self, key):
-        self._assert_not_closed()
-        return self._tree.delete(key)
-
-def connect(dbname):
-    try:
-        f = open(dbname, 'r+b')
-    except IOError:
-        fd = os.open(dbname, os.O_RDWR | os.O_CREAT)
-        f = os.fdopen(fd, 'r+b')
-    return DBDB(f)
