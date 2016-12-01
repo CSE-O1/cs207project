7e06e2a8606617959b762b00a982eff37992b952
minglong-cse2016 <minglong_wu@g.harvard.edu>
2016-12-01 16:14:23 -0500

    filefolder reorganization

--- storagemanager/FileStorageManager.py
+++ storagemanager/FileStorageManager.py
@@ -1,10 +1,54 @@
+""" Module for File Storage Manager.
+This module defines the FileStorageManager class.
+
+Classes
+-------
+FileStorageManager(StorageManagerInterface):
+    Define the FileStorageManager class for managing storage of timeseries data to database files.
+
+Methods
+-------
+__init__(self):
+    Constructor of FileStorageManager, loading IDs for timeseries in the database
+gen_id(self):
+    Generate an ID for a timeseries
+ts2nparray(self, t):
+    Convert a timeseries to a 2D np array in float64, returning a 2D np array
+     containing time and value
+store(self, id, t):
+    Store the given timeseries t in database using the input id, returning the same timeseries
+size(self, id):
+    Get the size of timeseries using the given ID, returning int size of the timeseries
+get(self, id):
+    Get the timeseries using an given ID, returning the timeseries of the given ID
+"""
+
 import numpy as np
 import os
 from SMInterface import StorageManagerInterface
-from timeseries.ArrayTimeSeries import ArrayTimeSeries
+from cs207project.timeseries.ArrayTimeSeries import ArrayTimeSeries
+
 
 class FileStorageManager(StorageManagerInterface):
+    """ FileStorageManager(StorageManagerInterface):
+    Define the FileStorageManager class for managing storage of timeseries data to database files.
 
+    Methods
+    -------
+    __init__(self):
+        Constructor of FileStorageManager, loading IDs for timeseries in the database
+    gen_id(self):
+        Generate an ID for a timeseries
+    ts2nparray(self, t):
+        Convert a timeseries to a 2D np array in float64, returning a 2D np array
+         containing time and value
+    store(self, id, t):
+        Store the given timeseries t in database using the input id, returning the same timeseries
+    size(self, id):
+        Get the size of timeseries using the given ID, returning int size of the timeseries
+    get(self, id):
+        Get the timeseries using an given ID, returning the timeseries of the given ID
+    """
     def __init__(self):
         """
         Constructor of FileStorageManager
@@ -12,25 +56,35 @@ class FileStorageManager(StorageManagerInterface):
         try:
             self._id = np.load('data/id.npz')
         except:
-            self._id = {}
+            self._id = set()
 
     def gen_id(self):
+        """
+        Generate an ID for a timeseries
+        :return: ID of a timeseries
+        """
         gid = 0
-        while gid in self._id: gid += 1
+        while gid in self._id:
+            gid += 1
         self._id.add(gid)
         return gid
 
     def ts2nparray(self, t):
+        """
+        Convert a timeseries to a 2D np array in float64
+        :param t: input timeseries
+        :return: a 2D np array containing time and value
+        """
         return np.vstack((t.times(), t.values())).astype(np.float64)
 
     def store(self, id, t):
         """
-        Store the given timeseries in database
-        :param id: Assigned ID of timeseries,
-        :param t: The timeseries to store
+        Store the given timeseries t in database using the input id
+        :param id: assigned ID of timeseries,
+        :param t: the timeseries to store
         :type id: int or string
         :type t: SizedContainerTimeSeriesInterface
-        :return: The same timeseries
+        :return: the same timeseries
         :rtype: SizedContainerTimeSeriesInterface
         """
         np.save('data/ts.'+str(id), self.ts2nparray(t))
@@ -40,10 +94,10 @@ class FileStorageManager(StorageManagerInterface):
 
     def size(self, id):
         """
-        Get the size of timeseries with given ID
+        Get the size of timeseries using the given ID
         :param id: ID of timeseries
         :type id: int or string
-        :return: Size of the timeseries
+        :return: size of the timeseries
         :rtype: int
         """
         if not os.path.exists('data/ts.'+str(id)+'.npz'):
@@ -52,10 +106,10 @@ class FileStorageManager(StorageManagerInterface):
 
     def get(self, id):
         """
-        Get the timeseries of given ID
+        Get the timeseries using an given ID
         :param id: ID of timeseries
         :type id: int or string
-        :return: Timeseries of given ID
+        :return: timeseries of the given ID
         :rtype: SizedContainerTimeSeriesInterface
         """
         if not os.path.exists('data/ts.' + str(id) + '.npz'):
--- storagemanager/SMInterface.py
+++ storagemanager/SMInterface.py
@@ -1,34 +1,67 @@
+""" Interface Module for Storage Manager.
+This module defines the abstract interface class StorageManagerInterface
+ for the FileStorageManager class.
+
+Classes
+-------
+StorageManagerInterface(abc.ABC):
+    Define the abstract class for the FileStorageManager class
+
+Methods
+-------
+store:
+    Abstract method for storing a timeseries in database
+size:
+    Abstract method for getting the size of a timeseries with the given ID
+get:
+    Abstract method for getting a timeseries with the given ID
+"""
+
 import abc
 
+
 class StorageManagerInterface(abc.ABC):
+    """
+    StorageManagerInterface(abc.ABC):
+    Define the abstract class for the FileStorageManager class
+
+    Methods
+    -------
+    store:
+        Abstract method for storing a timeseries in database
+    size:
+        Abstract method for getting the size of a timeseries with the given ID
+    get:
+        Abstract method for getting a timeseries with the given ID
+    """
     @abc.abstractmethod
     def store(self, id, t):
         """
         Store the given timeseries in database
-        :param id: Assigned ID of timeseries,
-        :param t: The timeseries to store
+        :param id: the assigned ID of the timeseries,
+        :param t: the timeseries to store
         :type id: int or string
         :type t: SizedContainerTimeSeriesInterface
-        :return: The same timeseries
+        :return: the same timeseries
         :rtype: SizedContainerTimeSeriesInterface
         """
 
     @abc.abstractmethod
     def size(self, id):
         """
-        Get the size of timeseries with given ID
+        Get the size of a timeseries with the given ID
         :param id: ID of timeseries
         :type id: int or string
-        :return: Size of the timeseries
+        :return: size of the timeseries
         :rtype: int
         """
 
     @abc.abstractmethod
     def get(self, id):
         """
-        Get the timeseries of given ID
+        Get a timeseries with the given ID
         :param id: ID of timeseries
         :type id: int or string
-        :return: Timeseries of given ID
+        :return: timeseries of the given ID
         :rtype: SizedContainerTimeSeriesInterface
         """
