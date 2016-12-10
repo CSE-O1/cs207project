74a40fcf57090118416cb07d92dc9aa1b3a43bf5
Qin Lyu <qlyu@g.harvard.edu>
2016-12-10 10:17:33 -0500

    merge bingfeng's code

---
+++ simsearch/SimilaritySearch.py
@@ -0,0 +1,91 @@
+from numpy.fft import fft, ifft, fftshift
+import numpy as np
+import timeseries.TimeSeries as ts
+from scipy.stats import norm
+
+def tsmaker(m, s, j):
+    t = np.arange(0.0, 1.0, 0.01)
+    v = norm.pdf(t, m, s) + j*np.random.randn(100)
+    return ts.TimeSeries(list(v), list(t))
+
+def random_ts(a):
+    t = np.arange(0.0, 1.0, 0.01)
+    v = a*np.random.random(100)
+    return ts.TimeSeries(list(v), list(t))
+
+def stand(x, m, s):
+    "standardize timeseries x by mean m and std deviation s"
+    return ts.TimeSeries((x.values() - m) / s, x.times())
+
+def standardize(x):
+    "simple standardize function"
+    return stand(x, x.mean(), x.std())
+
+def ccor(ts1, ts2):
+    "given two standardized time series, compute their cross-correlation using FFT"
+    f1 = fft(ts1.values())
+    f2 = np.conjugate(fft(ts2.values()))
+    cc = ifft(f1 * f2).real
+    return cc/(1.0 * len(ts1))
+
+# this is just for checking the max correlation with the
+#kernelized cross-correlation
+def max_corr_at_phase(ts1, ts2):
+    ccorts = ccor(ts1, ts2)
+    idx = np.argmax(ccorts)
+    maxcorr = ccorts[idx]
+    return idx, maxcorr
+
+#The equation for the kernelized cross correlation is given at
+#http://www.cs.tufts.edu/~roni/PUB/ecml09-tskernels.pdf
+#normalize the kernel there by np.sqrt(K(x,x)K(y,y)) so that the correlation
+#of a time series with itself is 1. We'll set the default multiplier to 1.
+def kernel_corr(ts1, ts2, mult=1):
+    "compute a kernelized correlation so that we can get a real distance"
+    cross_cor = ccor(ts1, ts2)
+    Kxy = np.sum(np.exp(mult * cross_cor))
+    Kxx = np.sum(np.exp(mult * ccor(ts1, ts1)))
+    Kyy = np.sum(np.exp(mult * ccor(ts2, ts2)))
+    return Kxy / np.sqrt(Kxx * Kyy)
+
+def kernel_dis(ts1, ts2, mult=1):
+    "compute kernel distance"
+    kernel_corr_val = kernel_corr(ts1, ts2, mult)
+    return 2 * (1 - kernel_corr_val)
+
+#this is for a quick and dirty test of these functions
+"""
+if __name__ == "__main__":
+    print("HI")
+    t1 = tsmaker(0.5, 0.1, 0.01)
+    t2 = tsmaker(0.5, 0.1, 0.01)
+    print(t1.mean(), t1.std(), t2.mean(), t2.std())
+    import matplotlib.pyplot as plt
+    plt.plot(t1)
+    plt.plot(t2)
+    plt.show()
+    standts1 = stand(t1, t1.mean(), t1.std())
+    standts2 = stand(t2, t2.mean(), t2.std())
+
+    idx, mcorr = max_corr_at_phase(standts1, standts2)
+    print(idx, mcorr)
+    sumcorr = kernel_corr(standts1, standts2, mult=10)
+    print(sumcorr)
+    t3 = random_ts(2)
+    t4 = random_ts(3)
+    plt.plot(t3)
+    plt.plot(t4)
+    plt.show()
+    standts3 = stand(t3, t3.mean(), t3.std())
+    standts4 = stand(t4, t4.mean(), t4.std())
+    idx, mcorr = max_corr_at_phase(standts3, standts4)
+    print(idx, mcorr)
+    sumcorr = kernel_corr(standts3, standts4, mult=10)
+    print(sumcorr)
+
+    print("test:")
+    test1 = ts.TimeSeries([1,2], [3, 4])
+    test2 = ts.TimeSeries([1, 2], [3, 4])
+    test_sumcorr = kernel_corr(test1, test2, mult=10)
+    print(test_sumcorr)
+"""
---
+++ simsearch/findKthSimilarity.py
@@ -0,0 +1,80 @@
+import sys
+import simsearch.TimeSeries as ts
+import simsearch.SimilaritySearch as ss
+import numpy as np
+import simsearch.btreeDB as btreeDB
+
+
+def load_ts_data(file_name):
+    "load timeseries data form given file name"
+    ts_raw_data = np.loadtxt(file_name, delimiter=' ')
+    ts_data = ts.TimeSeries(ts_raw_data[:, 1], ts_raw_data[:, 0])
+    return ts_data
+
+
+def max_similarity_search(input_ts):
+    """
+    find the most similar vantage point of the target TS
+    return tuple (minimum distance, vantage point, timeseries file name)
+    """
+    comp_ts = input_ts
+    std_comp_ts = ss.standardize(comp_ts)
+    min_dis = float('inf')
+    min_db_name = ""
+    min_ts_file_name = ""
+    for i in range(20):
+        db_name = "vpDB/db_" + str(i) + ".dbdb"
+        db = btreeDB.connect(db_name)
+        ts_data_file_name = db.get(0)
+        vp_ts = load_ts_data(ts_data_file_name)
+        std_vp_ts = ss.standardize(vp_ts)
+        curr_dis = ss.kernel_dis(std_vp_ts, std_comp_ts)
+        if min_dis > curr_dis:
+            min_dis = curr_dis
+            min_db_name = db_name
+            min_ts_file_name = ts_data_file_name
+    return min_dis, min_db_name, min_ts_file_name
+
+
+def kth_similarity_search(input_ts, min_dis, min_db_name, k=1):
+    """
+    find the most kth similar timeseries data
+    return file names in an array
+    """
+    db = btreeDB.connect(min_db_name)
+    keys, ts_file_names = db.get_smaller_nodes(2.0 * min_dis)
+    ts_file_lens = len(ts_file_names)
+    kth_ts_list = []
+    for i in range(ts_file_lens):
+        res_ts = load_ts_data(ts_file_names[i])
+        std_res_ts = ss.standardize(res_ts)
+        curr_dis = ss.kernel_dis(std_res_ts, input_ts)
+        kth_ts_list.append((curr_dis, ts_file_names[i]))
+    # sort in ascending order by distance
+    kth_ts_list.sort(key=lambda kv: kv[0])
+    # ill situation
+    if (len(kth_ts_list) <= k):
+        return kth_ts_list
+    else:
+        return kth_ts_list[: k]
+
+
+def find_kth_similarity(input_file_name, k):
+    """
+    main function
+    :param input_file_name: input TS file name to be compared
+    :param k: top k similar nodes compared to input TS file
+    :return: print the most kth similar timeseries file name
+    """
+    input_ts = load_ts_data(input_file_name)
+    min_dis, min_db_name, min_ts_file_name = max_similarity_search(input_ts)
+    kth_similarity_list = kth_similarity_search(input_ts, min_dis, min_db_name, k)
+    print("The %dth closest TimeSeries data of %s is:" % (k, input_file_name))
+    for i in range(len(kth_similarity_list)):
+        print("No.%d %s" % (i + 1, kth_similarity_list[i][1]))
+
+
+if __name__ == "__main__":
+    input_file_name = sys.argv[1]
+    k = int(sys.argv[2])
+    find_kth_similarity(input_file_name, k)
